43976c878c7fc4553673d3e5db1bcd668961ca3a
Mukul Arora <amukul864@gmail.com>
2024-06-25 18:05:23 +0530

    changed some ui

--- src/app/components/ControlButtons/ControlButtons.tsx
+++ src/app/components/ControlButtons/ControlButtons.tsx
@@ -2,94 +2,81 @@ import { useContext, useEffect, useState } from "react";
 import styles from "./ControlButtons.module.css";
 import VideoContext from "../../store/video-context";
 import Button from "../ui/Button";
+import { useRouter } from "next/navigation";
 
 const ControlButtons = () => {
-	const ctx = useContext(VideoContext);
-	const [minTimeMet, setMinTimeMet] = useState<boolean>(false);
+  const ctx = useContext(VideoContext);
+  const [minTimeMet, setMinTimeMet] = useState<boolean>(false);
+  const router = useRouter();
 
-	useEffect(() => {
-		const minDuration = 30;
-		const maxDuration = 120;
-		if (ctx.time < 1000 * minDuration) {
-			setMinTimeMet(false);
-		} else {
-			setMinTimeMet(true);
-		}
-		if (ctx.time > 1000 * maxDuration) {
-			ctx.stopRecording();
-		}
-	}, [ctx.time]);
+  useEffect(() => {
+    if (
+      process.env.NEXT_PUBLIC_MIN_DURATION &&
+      process.env.NEXT_PUBLIC_MAX_DURATION
+    ) {
+      const minDuration = parseInt(process.env.NEXT_PUBLIC_MIN_DURATION);
+      const maxDuration = parseInt(process.env.NEXT_PUBLIC_MAX_DURATION);
+      if (ctx.time < 1000 * minDuration) {
+        setMinTimeMet(false);
+      } else {
+        setMinTimeMet(true);
+      }
+      if (ctx.time > 1000 * maxDuration) {
+        ctx.stopRecording();
+        ctx.confirmRecording();
+        router.push("/review");
+      }
+    }
+  }, [ctx.time]);
 
-	return (
-		<>
-			<div className={styles.controlButtons}>
-				{!(
-					ctx.recording ||
-					ctx.paused ||
-					ctx.playing ||
-					!!ctx.videoBlob
-				) && (
-					<Button
-						onClick={ctx.startRecording}
-						disabled={
-							ctx.recording ||
-							ctx.paused ||
-							ctx.playing ||
-							!!ctx.videoBlob
-						}
-						className={styles.controlButton}
-					>
-						Start Recording
-					</Button>
-				)}
-				{!(!ctx.recording || ctx.paused) && (
-					<Button
-						onClick={ctx.pauseRecording}
-						disabled={!ctx.recording || ctx.paused}
-						className={styles.controlButton}
-					>
-						Pause Recording
-					</Button>
-				)}
-				{!(!ctx.recording || !ctx.paused) && (
-					<Button
-						onClick={ctx.resumeRecording}
-						disabled={!ctx.recording || !ctx.paused}
-						className={styles.controlButton}
-					>
-						Resume Recording
-					</Button>
-				)}
-				{!(!ctx.recording || !minTimeMet) && (
-					<Button
-						onClick={ctx.stopRecording}
-						disabled={!ctx.recording || !minTimeMet}
-						className={styles.controlButton}
-					>
-						Stop Recording
-					</Button>
-				)}
-				{!!ctx.videoBlob && (
-					<Button
-						onClick={ctx.resetRecording}
-						disabled={!ctx.videoBlob}
-						className={styles.controlButton}
-					>
-						Record Again
-					</Button>
-				)}
-				{!!ctx.videoBlob && (
-					<Button
-						onClick={ctx.confirmRecording}
-						disabled={!ctx.videoBlob}
-						className={styles.controlButton}
-					>
-						Confirm
-					</Button>
-				)}
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className={styles.controlButtons}>
+        {!(ctx.recording || ctx.paused || ctx.playing || !!ctx.videoBlob) && (
+          <Button
+            onClick={ctx.startRecording}
+            disabled={
+              ctx.recording || ctx.paused || ctx.playing || !!ctx.videoBlob
+            }
+            className={styles.controlButton}
+          >
+            Start Recording
+          </Button>
+        )}
+        {!(!ctx.recording || ctx.paused) && (
+          <Button
+            onClick={ctx.pauseRecording}
+            disabled={!ctx.recording || ctx.paused}
+            className={styles.controlButton}
+          >
+            Pause Recording
+          </Button>
+        )}
+        {!(!ctx.recording || !ctx.paused) && (
+          <Button
+            onClick={ctx.resumeRecording}
+            disabled={!ctx.recording || !ctx.paused}
+            className={styles.controlButton}
+          >
+            Resume Recording
+          </Button>
+        )}
+        {!(!ctx.recording || !minTimeMet) && (
+          <Button
+            onClick={() => {
+              ctx.stopRecording();
+              ctx.confirmRecording();
+              router.push("/review");
+            }}
+            disabled={!ctx.recording || !minTimeMet}
+            className={styles.controlButton}
+          >
+            Stop Recording
+          </Button>
+        )}
+      </div>
+    </>
+  );
 };
 
 export default ControlButtons;
--- src/app/components/VideoScript/VideoScript.tsx
+++ src/app/components/VideoScript/VideoScript.tsx
@@ -4,78 +4,75 @@ import { useState, useEffect } from "react";
 import LoadingSpinner from "../ui/LoadingSpinner";
 
 const VideoScript = () => {
-	const [textSize, setTextSize] = useState<number>(16);
-	const [data, setData] = useState<{ script: string } | null>(null);
+  const [textSize, setTextSize] = useState<number>(16);
+  const [data, setData] = useState<{ data: string } | null>(null);
 
-	const increaseSizeByTwo = () => {
-		if (textSize < 36) {
-			setTextSize((state) => state + 2);
-		}
-	};
+  const increaseSizeByTwo = () => {
+    if (textSize < 36) {
+      setTextSize((state) => state + 2);
+    }
+  };
 
-	const decreaseSizeByTwo = () => {
-		if (textSize > 12) {
-			setTextSize((state) => state - 2);
-		}
-	};
+  const decreaseSizeByTwo = () => {
+    if (textSize > 12) {
+      setTextSize((state) => state - 2);
+    }
+  };
 
-	useEffect(() => {
-		const fetchData = async () => {
-			try {
-				const response = await fetch(
-					`${process.env.NEXT_PUBLIC_API_URL}/videos/script`,
-					{
-						method: "GET",
-						credentials: "include",
-						headers: {
-							"Content-Type": "application/json",
-						},
-					}
-				);
-				if (!response.ok) {
-					throw new Error("Network response was not ok");
-				}
-				const json = await response.json();
-				setData(json);
-			} catch (error) {
-				console.log(error);
-			}
-		};
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_API_URL}/videos/scripts`,
+          {
+            method: "GET",
+            credentials: "include",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const json = await response.json();
+        setData(json);
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
-		fetchData();
-	}, []);
+    fetchData();
+  }, []);
 
-	return (
-		<div className={styles.videoContent}>
-			<div className={styles.heading}>
-				<div className={styles.headingText}>Script</div>
-				<div className={styles.scriptSize}>
-					<p className={styles.textSizeHeading}>Font Size</p>
-					<Button
-						className={styles.scriptSizeButton}
-						onClick={increaseSizeByTwo}
-					>
-						+
-					</Button>
-					<p className={styles.textSize}>{textSize}</p>
-					<Button
-						className={styles.scriptSizeButton}
-						onClick={decreaseSizeByTwo}
-					>
-						-
-					</Button>
-				</div>
-			</div>
-			<div className={styles.hr}></div>
-			<div
-				className={styles.script}
-				style={{ fontSize: `${textSize}px` }}
-			>
-				{data && <div>{data.script}</div>}
-				{!data && <LoadingSpinner />}
-			</div>
-		</div>
-	);
+  return (
+    <div className={styles.videoContent}>
+      <div className={styles.heading}>
+        <div className={styles.headingText}>Script</div>
+        <div className={styles.scriptSize}>
+          <p className={styles.textSizeHeading}>Font Size</p>
+          <Button
+            className={styles.scriptSizeButton}
+            onClick={increaseSizeByTwo}
+          >
+            +
+          </Button>
+          <p className={styles.textSize}>{textSize}</p>
+          <Button
+            className={styles.scriptSizeButton}
+            onClick={decreaseSizeByTwo}
+          >
+            -
+          </Button>
+        </div>
+      </div>
+      <div className={styles.hr}></div>
+      <div className={styles.script} style={{ fontSize: `${textSize}px` }}>
+        {data && <div>{data.data}</div>}
+        {!data && <LoadingSpinner />}
+      </div>
+    </div>
+  );
 };
 
 export default VideoScript;
